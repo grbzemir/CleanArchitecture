7e9583789af06706754c2bd58e5c20922d5a00d8
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-08-20 12:31:32 +0300

    feat: entity

---
+++ src/Core/TestApp.Domain/Entities/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Domain.Common;
+
+namespace TestApp.Domain.Entities
+{
+	public class Test: BaseEntity
+	{
+		public string Name { get; set; }
+		public decimal Value { get; set; }	
+		public int Quantity { get; set; }
+	}
+}
