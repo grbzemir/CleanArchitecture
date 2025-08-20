1c1afd51a87f74f86524a1574529f6060f1c56a4
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-08-20 13:44:41 +0300

    feat: test controller and registration

---
+++ WebApi/TestApp.WebApi/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestApp.Application.Dto;
+using TestApp.Application.Interfaces.Repository;
+
+namespace TestApp.WebApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TestController : ControllerBase
+	{
+       private readonly ITestRepository testRepository;
+        public TestController(ITestRepository testRepository)
+        {
+            this.testRepository = testRepository;
+        }
+		[HttpGet]
+		public async Task<IActionResult> Get()
+		{
+			var allList = await testRepository.GetAllAsync();
+			var result = allList.Select(i => new TestViewDto()
+			{
+				Id = i.Id,
+				Name = i.Name,
+			}).ToList();
+
+			return Ok(result);
+		}
+	}
+}
---
+++ src/Infrastructure/TestApp.Persistence/ServiceRegistration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using TestApp.Application.Interfaces.Repository;
+using TestApp.Persistence.Context;         
+using TestApp.Persistence.Repositories;     
+
+namespace TestApp.Application
+{
+	public static class ServiceRegistration
+	{
+		public static void AddPersistenceServices(this IServiceCollection serviceCollection)
+		{
+			serviceCollection.AddDbContext<ApplicationDbContext>(opt =>
+				opt.UseInMemoryDatabase("memoryDb"));
+
+			serviceCollection.AddTransient<ITestRepository, TestRepository>();
+		}
+	}
+}
