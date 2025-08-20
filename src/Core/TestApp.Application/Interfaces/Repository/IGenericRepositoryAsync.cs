1608959c7470370ed8ca939dc8fa61e04d9b00d5
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-08-20 12:49:37 +0300

    feat: generic T

---
+++ src/Core/TestApp.Application/Interfaces/Repository/IGenericRepositoryAsync.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Domain.Common;
+using TestApp.Domain.Entities;
+
+namespace TestApp.Application.Interfaces.Repository
+{
+	public interface IGenericRepositoryAsync<T> where T : BaseEntity
+	{
+		Task<List<T>> GetAllAsync();
+		Task<T> GetByIdAsync(Guid id);
+		Task<T> AddAsync(T entity);
+	}
+}
---
+++ src/Core/TestApp.Application/Interfaces/Repository/ITestRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestApp.Domain.Entities;
+
+namespace TestApp.Application.Interfaces.Repository
+{
+	public interface ITestRepository: IGenericRepositoryAsync<Test>
+	{
+		
+	}
+}
